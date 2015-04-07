9d084dd0be506a34400e0e8c6824d843a4921598
Dan Burton <danburton.email@gmail.com>
2015-04-06 17:59:19 -0700

    Added stackage-sandbox delete and upgrade

--- main/Sandbox.hs
+++ main/Sandbox.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Control.Exception (bracket_)
 import Control.Monad (when)
 import Data.Maybe
 import Data.Monoid
@@ -9,11 +10,11 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Filesystem.Path.CurrentOS as Path
-import Filesystem (isFile, createTree)
+import Filesystem
 import Options.Applicative hiding (header, progDesc)
 import Stackage.CLI
 import System.Environment (getEnv)
-import System.Exit (exitFailure)
+import System.Exit (exitFailure, exitSuccess)
 import System.Process (callProcess, readProcess)
 
 type Snapshot = Text
@@ -23,6 +24,8 @@ data Action
   | PackageDb
   | List (Maybe Package)
   | Unregister Package
+  | Delete (Maybe Snapshot)
+  | Upgrade (Maybe Snapshot)
   -- TODO: other commands
 
 version :: String
@@ -51,20 +54,32 @@ listDesc = "Calls `ghc-pkg list` with the sandbox package-db"
 unregisterDesc :: String
 unregisterDesc = "Calls `ghc-pkg unregister PACKAGE with the sandbox package-db"
 
+deleteDesc :: String
+deleteDesc = "Deletes cabal.config and cabal.sandbox.config. "
+  <> "If provided with a SNAPSHOT, instead deletes that snapshot's folder."
+
+upgradeDesc :: String
+upgradeDesc = "Upgrade to the given SNAPSHOT. Defaults to the latest LTS."
+
 subcommands = mconcat
   [ simpleCommand "init" "Init" Init (optional snapshotParser)
   , simpleCommand "package-db" packageDbDesc (const PackageDb) (pure ())
   , simpleCommand "list" listDesc List (optional packageParser)
   , simpleCommand "unregister" unregisterDesc Unregister packageParser
+  , simpleCommand "delete" deleteDesc Delete (optional snapshotParser)
+  , simpleCommand "upgrade" upgradeDesc Upgrade (optional snapshotParser)
   -- TODO: other commands
   ]
+
+toText' :: Path.FilePath -> IO Text
+toText' p = case toText p of
+  Left e -> fail $ "Couldn't decode path: " <> T.unpack e
+  Right t -> return t
 
 -- TODO: handle errors gracefully
 cabalSandboxInit :: Path.FilePath -> IO ()
 cabalSandboxInit dir = do
-  dirText <- case toText dir of
-    Left e -> fail $ "Couldn't decode path: " <> T.unpack e
-    Right e -> return e
+  dirText <- toText' dir
   let args =
         [ "sandbox"
         , "init"
@@ -89,8 +104,28 @@ parseConfigSnapshot = do
 snapshotEq :: Snapshot -> Snapshot -> Bool
 snapshotEq short full = T.isPrefixOf (T.replace "/" "-" short) full
 
+-- TODO: verify things about the packages in the sandbox
 sandboxVerify :: IO ()
-sandboxVerify = putStrLn "TODO: stackage sandbox verify"
+sandboxVerify = do
+  cabalConfigExists <- isFile "cabal.config"
+  cabalSandboxConfigExists <- isFile "cabal.sandbox.config"
+  if (cabalConfigExists && cabalSandboxConfigExists)
+    then do
+      packageDb <- getPackageDb
+      snapshot <- parseConfigSnapshot
+      snapshotDir <- getSnapshotDir snapshot
+      snapshotDirText <- toText' snapshotDir
+      when (not $ T.isPrefixOf snapshotDirText packageDb) $ do
+        putStrLn "verify: package db isn't in the expected location:"
+        T.putStrLn $ "dir: " <> snapshotDirText
+        T.putStrLn $ "db: " <> packageDb
+        exitFailure
+    else do
+      when (not cabalConfigExists) $
+        putStrLn "verify: cabal.config not present"
+      when (not cabalSandboxConfigExists) $
+        putStrLn "verify: cabal.sandbox.config not present"
+      exitFailure
 
 -- TODO: handle errors gracefully
 getGhcVersion :: IO Text
@@ -126,14 +161,19 @@ sandboxInit msnapshot = do
 
   T.putStrLn $ "Initializing at snapshot: " <> snapshot
 
+  dir <- getSnapshotDir snapshot
+  createTree dir
+  cabalSandboxInit dir
+  sandboxVerify
+
+getSnapshotDir :: Snapshot -> IO Path.FilePath
+getSnapshotDir snapshot = do
   -- TODO: handle env exceptions
   home <- T.pack <$> getEnv "HOME"
   ghcVersion <- getGhcVersion
   let dir = Path.fromText home </> ".stackage" </> "sandboxes"
         </> Path.fromText ghcVersion </> Path.fromText snapshot
-  createTree dir
-  cabalSandboxInit dir
-  sandboxVerify
+  return dir
 
 -- copied from Purge.hs, tweaked
 -- TODO: remove duplication
@@ -148,18 +188,23 @@ parsePackageDb = do
     else
       return Nothing
 
-
 getPackageDb :: IO Text
 getPackageDb = parsePackageDb >>= \mdb -> case mdb of
+  Just packageDb -> return packageDb
+  Nothing -> fail "Couldn't find sandbox package-db"
+
+getPackageDbArg :: IO Text
+getPackageDbArg = parsePackageDb >>= \mdb -> case mdb of
   Just packageDb -> return $ "--package-db=" <> packageDb
   Nothing -> return ""
 
+
 printPackageDb :: IO ()
-printPackageDb = getPackageDb >>= T.putStrLn
+printPackageDb = getPackageDbArg >>= T.putStrLn
 
 ghcPkgList :: Maybe Package -> IO ()
 ghcPkgList mPackage = do
-  packageDb <- getPackageDb
+  packageDb <- getPackageDbArg
   let args
         = ["list"]
        <> case mPackage of
@@ -170,8 +215,63 @@ ghcPkgList mPackage = do
 
 ghcPkgUnregister :: Package -> IO ()
 ghcPkgUnregister package = do
-  packageDb <- getPackageDb
+  packageDb <- getPackageDbArg
   callProcess "ghc-pkg" ["unregister", T.unpack package, T.unpack packageDb]
+
+sandboxDelete :: IO ()
+sandboxDelete = do
+  cabalConfigExists <- isFile "cabal.config"
+  when cabalConfigExists $ do
+    removeFile "cabal.config"
+  cabalSandboxConfigExists <- isFile "cabal.sandbox.config"
+  when cabalSandboxConfigExists $ do
+    removeFile "cabal.sandbox.config"
+
+sandboxDeleteSnapshot :: Snapshot -> IO ()
+sandboxDeleteSnapshot snapshot
+  = getSnapshotDir snapshot >>= removeTree
+
+-- Find the canonical name for a snapshot by looking it up on stackage.org.
+-- This can change over time. e.g. "lts" used to mean lts-1.0.
+downloadSnapshot :: Maybe Snapshot -> IO Snapshot
+downloadSnapshot mSnapshot = do
+  workingDir <- getWorkingDirectory
+  let tempDir = ".stackage-sandbox-tmp"
+      enterTempDir = do
+        createDirectory False tempDir
+        setWorkingDirectory tempDir
+      exitTempDir = do
+        setWorkingDirectory workingDir
+        removeTree tempDir
+  bracket_ enterTempDir exitTempDir $ do
+    runStackagePlugin "init" (maybeToList mSnapshot)
+    parseConfigSnapshot
+
+
+sandboxUpgrade :: Maybe Snapshot -> IO ()
+sandboxUpgrade mSnapshot = do
+  cabalConfigExists <- isFile "cabal.config"
+  cabalSandboxConfigExists <- isFile "cabal.sandbox.config"
+
+  mConfigSnapshot <- if cabalConfigExists
+    then Just <$> parseConfigSnapshot
+    else return Nothing
+
+  snapshot <- downloadSnapshot mSnapshot
+
+  when (Just snapshot == mConfigSnapshot && cabalSandboxConfigExists) $ do
+    packageDb <- getPackageDb
+    snapshotDir <- getSnapshotDir snapshot
+    snapshotDirText <- toText' snapshotDir
+    if T.isPrefixOf snapshotDirText packageDb
+      then do
+        -- TODO: more verification
+        T.putStrLn $ "Already at snapshot: " <> snapshot
+        exitSuccess
+      else return ()
+
+  sandboxDelete
+  sandboxInit mSnapshot
 
 main = do
   ((), action) <- simpleOptions
@@ -185,4 +285,7 @@ main = do
     PackageDb -> printPackageDb
     List mPackage -> ghcPkgList mPackage
     Unregister package -> ghcPkgUnregister package
-    -- TODO: other commands
+    Delete mSnapshot -> case mSnapshot of
+      Just snapshot -> sandboxDeleteSnapshot snapshot
+      Nothing -> sandboxDelete
+    Upgrade mSnapshot -> sandboxUpgrade mSnapshot
