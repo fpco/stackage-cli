16f59a732cf70a4845b5f90d28fd9eda6c4d1a05
Dan Burton <danburton.email@gmail.com>
2015-04-09 10:02:02 -0700

    Add user agent to requests (#14)

--- main/Init.hs
+++ main/Init.hs
@@ -10,9 +10,13 @@ import Stackage.CLI
 import Options.Applicative (Parser)
 import Options.Applicative.Builder (strArgument, metavar, value)
 import Data.Monoid
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Data.Typeable (Typeable)
 import Network.HTTP.Client
 import Network.HTTP.Types.Status (statusCode)
+import Network.HTTP.Types.Header (hUserAgent)
 import qualified Data.ByteString.Lazy as LBS
 import System.Exit (exitFailure)
 import System.Environment (getArgs)
@@ -29,6 +33,18 @@ data InitException
   deriving (Show, Typeable)
 instance Exception InitException
 
+version :: String
+version = "0.1"
+
+header :: String
+header = "Initializes cabal.config"
+
+progDesc :: String
+progDesc = header
+
+userAgent :: Text
+userAgent = "stackage-init/" <> T.pack version
+
 snapshotParser :: Parser Snapshot
 snapshotParser = strArgument mods where
   mods = (metavar "SNAPSHOT" <> value "lts")
@@ -40,6 +56,8 @@ snapshotReq :: Snapshot -> IO Request
 snapshotReq snapshot = case parseUrl (toUrl snapshot) of
   Left _ -> throwIO $ InvalidSnapshot
   Right req -> return req
+    { requestHeaders = [(hUserAgent, T.encodeUtf8 userAgent)]
+    }
 
 downloadSnapshot :: Snapshot -> IO LBS.ByteString
 downloadSnapshot snapshot = withManager defaultManagerSettings $ \manager -> do
@@ -62,15 +80,6 @@ initSnapshot snapshot = do
   configExists <- isFile "cabal.config"
   when configExists $ throwIO $ CabalConfigExists
   downloadSnapshot snapshot >>= LBS.writeFile "cabal.config"
-
-version :: String
-version = "0.1"
-
-header :: String
-header = "Initializes cabal.config"
-
-progDesc :: String
-progDesc = header
 
 handleInitExceptions :: Snapshot -> InitException -> IO ()
 handleInitExceptions snapshot e = hPutStrLn stderr (err e) >> exitFailure where
