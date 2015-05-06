17b9c6e241f02f9a92ef012a4e509240084c801e
Dan Burton <danburton.email@gmail.com>
2015-05-06 13:21:24 -0700

    Add temporary post-install notice

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,22 @@
 import Distribution.Simple
-main = defaultMain
+
+main = defaultMainWithHooks simpleUserHooks
+  { postInst = \_ _ _ _ -> putStrLn notice
+  }
+
+notice =
+  "                                                                               \n\
+  \- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+  \                                                                               \n\
+  \              Notice: some stackage plugins have moved!                        \n\
+  \                                                                               \n\
+  \      cabal install stackage-cabal                                             \n\
+  \        # will get you the plugins:                                            \n\
+  \          # stackage-init                                                      \n\
+  \          # stackage-purge                                                     \n\
+  \          # stackage-upgrade                                                   \n\
+  \                                                                               \n\
+  \      cabal install stackage-sandbox                                           \n\
+  \        # will get you the stackage-sandbox plugin                             \n\
+  \                                                                               \n\
+  \- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
